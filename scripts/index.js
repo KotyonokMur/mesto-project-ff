342c7f8ad0a38a70f6f5a775b6be80fb673837e4
Dmitriy Kuznetsov <dmitriy.kuznetsov.2002@list.ru>
2024-02-27 22:56:00 +0300

    ver1.1

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,36 @@
+//Строгий режим
+'use strict';
+
 // @todo: Темплейт карточки
-const cardTemplate = document.querySelector("#card-template").content;
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
-const content = document.querySelector(".places");
-const cardList = content.querySelector(".places__list");
+const content = document.querySelector('.places');
+const cardList = content.querySelector('.places__list');
 
 // @todo: Функция удаления карточки
 function deleteCard(event) {
-  const deleteItem = event.target.closest(".card");
-  
+  const deleteItem = event.target.closest('.card');
   deleteItem.remove();
 }
 
 // @todo: Функция создания карточки
-function createCard(name, link) {
-  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
-  const deleteButton = cardElement.querySelector(".card__delete-button");
+function createCard(name, link, deleteCallback) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
 
-  cardElement.querySelector(".card__title").textContent = name;
-  cardElement.querySelector(".card__image").src = link;
+  cardElement.querySelector('.card__title').textContent = name;
+  cardElement.querySelector('.card__image').src = link;
+  cardElement.querySelector('.card__image').alt = 'Изображение ' + name;
 
-  deleteButton.addEventListener("click", deleteCard);
+  deleteButton.addEventListener('click', deleteCallback);
 
   return cardElement;
 }
 
 // @todo: Вывести карточки на страницу
-for (let i = 0; i < initialCards.length; i = i + 1){
-  cardElement = createCard(initialCards[i].name, initialCards[i].link);
- 
+initialCards.forEach(item => {
+  const cardElement = createCard(item.name, item.link, deleteCard);
   cardList.append(cardElement);
-}
+});
+

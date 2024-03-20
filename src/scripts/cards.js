a8833138e1eb5c6fecd791df811cf244b552f436
Dmitriy <dmitriy.kuznetsov.2002@list.ru>
2024-03-20 23:01:55 +0300

    build2

--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -1,26 +1,50 @@
+// @todo: Карточки
 export const initialCards = [
-    {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-    },
-    {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-    },
-    {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-    },
-    {
-      name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-    },
-    {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-    },
-    {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-    }
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
 ];
+
+// @todo: темлпейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+
+// @todo: Функция удаления карточки
+export function deleteCard(event) {
+  const deleteItem = event.target.closest(".card");
+  deleteItem.remove();
+}
+
+// @todo: Функция создания карточки
+export function createCard(name, link, deleteCallback) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardElement.querySelector(".card__title").textContent = name;
+  cardElement.querySelector(".card__image").src = link;
+  cardElement.querySelector(".card__image").alt = "Изображение " + name;
+
+  deleteButton.addEventListener("click", deleteCallback);
+
+  return cardElement;
+}
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,39 +1,14 @@
-//Строгий режим
-'use strict';
+"use strict"; // Строгий режим
 
-import '../pages/index.css';//Подключение главного файла стилей.]
-import { initialCards } from './cards.js';
-
-// @todo: Темплейт карточки
-const cardTemplate = document.querySelector('#card-template').content;
+import "../pages/index.css"; // Подключение главного файла стилей.]
+import { initialCards, createCard, deleteCard, consolelog } from "./cards.js"; // Импорт функций карточек
 
 // @todo: DOM узлы
-const content = document.querySelector('.places');
-const cardList = content.querySelector('.places__list');
-
-// @todo: Функция удаления карточки
-function deleteCard(event) {
-  const deleteItem = event.target.closest('.card');
-  deleteItem.remove();
-}
-
-// @todo: Функция создания карточки
-function createCard(name, link, deleteCallback) {
-  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
-  const deleteButton = cardElement.querySelector('.card__delete-button');
-
-  cardElement.querySelector('.card__title').textContent = name;
-  cardElement.querySelector('.card__image').src = link;
-  cardElement.querySelector('.card__image').alt = 'Изображение ' + name;
-
-  deleteButton.addEventListener('click', deleteCallback);
-
-  return cardElement;
-}
+const content = document.querySelector(".places");
+const cardList = content.querySelector(".places__list");
 
 // @todo: Вывести карточки на страницу
-initialCards.forEach(item => {
+initialCards.forEach((item) => {
   const cardElement = createCard(item.name, item.link, deleteCard);
   cardList.append(cardElement);
 });
-
